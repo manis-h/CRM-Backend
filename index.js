938c47ff7ae2142182dd87bcfcc32aa9233f0d68
Manish-o-1 <manish@only1loan.com>
2024-09-19 18:26:56 +0530

    okokn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import cors from "cors"; // Import cors
 import connectDB from "./config/db.js";
 import "dotenv/config.js";
 import morgan from "morgan";
-import leadRouter from "./routes/LeadsRouter.js"; // Import routes
+// import leadRouter from "./routes/LeadsRouter.js"; // Import routes
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 connectDB();
 
 const app = express();
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
     res.send("API is running.......");
 });
 
-app.use("/api/leads", leadRouter); // Use the user routes
+// app.use("/api/leads", leadRouter); // Use the user routes
 
 app.use(notFound);
 app.use(errorHandler);
