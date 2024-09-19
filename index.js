d8907fca15110057f71d55c567a614ae0a2385ab
Manish-o-1 <manish@only1loan.com>
2024-09-19 18:46:53 +0530

    shouldworkleads

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors"; // Import cors
 import connectDB from "./config/db.js";
 import "dotenv/config.js";
 import morgan from "morgan";
-// import leadRouter from "./routes/LeadsRouter.js"; // Import routes
+import leadRouter from "./routes/LeadsRouter.js"; // Import routes
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
 const PORT = 3000;
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
     res.send("API is running.......");
 });
 
-// app.use("/api/leads", leadRouter); // Use the user routes
+app.use("/api/leads", leadRouter); // Use the user routes
 
 app.use(notFound);
 app.use(errorHandler);
--- routes/LeadsRouter.js
+++ routes/LeadsRouter.js
@@ -1,23 +1,96 @@
-// import express from "express";
-// const router = express.Router();
+import express from "express";
+const router = express.Router();
 
-// import { login, logout, register } from "../Controllers/usercontrollers.js";
-// import { createLead, getAllLeads } from "../controllers/leads.js";
-// // import { authenticateToken } from "../middleware/authMiddleware.js";
+import { login, logout, register } from "../Controllers/usercontrollers.js";
+// import { createLead, getAllLeads } from "../controllers/leads";
+import asyncHandler from "../middleware/asyncHandler.js";
+import Lead from "../models/Leads.js";
 
-// // Route to register a new user
-// router.route("/register").post(register);
+// @desc Create loan leads
+// @route POST /api/leads
+// @access Public
 
-// // Route to login
-// router.route("/login").post(login);
-// router.route("/logout").post(logout);
-// // router.get("/protected-route", authenticateToken, (req, res) => {
-// //     res.status(200).json({
-// //         message: "You have access to protected data!",
-// //         user: req.user,
-// //     });
-// // });
-// // Other routes
-// router.route("/").post(createLead).get(getAllLeads);
+const createLead = asyncHandler(async (req, res) => {
+    const {
+        fName,
+        mName,
+        lName,
+        gender,
+        dob,
+        adhaar,
+        pan,
+        mobile,
+        alternateMobile,
+        personalEmail,
+        officeEmail,
+        loanAmount,
+        salary,
+        pinCode,
+        state,
+        city,
+    } = req.body;
+    const newLead = await Lead.create({
+        fName,
+        mName: mName ?? "",
+        lName: lName ?? "",
+        gender,
+        dob,
+        adhaar,
+        pan,
+        mobile,
+        alternateMobile,
+        personalEmail,
+        officeEmail,
+        loanAmount,
+        salary,
+        pinCode,
+        state,
+        city,
+    });
+    // const savedUserDetails = await newUserDetails.save();
+    res.status(201).json(newLead);
+});
 
-// export default router;
+// @desc Get all leads
+// @route GET /api/leads
+// @access Private
+const getAllLeads = async (req, res) => {
+    try {
+        // const page = parseInt(req.query.page) || 1; // current page
+        // const limit = parseInt(req.query.limit) || 10; // items per page
+        // const skip = (page - 1) * limit;
+
+        const leads = await Lead.find({}); //.skip(skip).limit(limit);
+        const totalLeads = await Lead.countDocuments();
+
+        res.status(200).json({
+            totalLeads,
+            // totalPages: Math.ceil(totalLeads / limit),
+            // currentPage: page,
+            leads,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error retrieving user details",
+            error,
+        });
+    }
+};
+// import { authenticateToken } from "../middleware/authMiddleware.js";
+
+// Route to register a new user
+router.route("/register").post(register);
+
+// Route to login
+router.route("/login").post(login);
+router.route("/logout").post(logout);
+// router.get("/protected-route", authenticateToken, (req, res) => {
+//     res.status(200).json({
+//         message: "You have access to protected data!",
+//         user: req.user,
+//     });
+// });
+// Other routes
+router.route("/").post(createLead).get(getAllLeads);
+
+export default router;
