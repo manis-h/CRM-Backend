1c317f19b2c50d848d845a9e33c726dbb83ff8d0
AjayOnly1Loan <ajay@only1loan.com>
2024-10-29 13:34:04 +0530

    commit

--- Controllers/leads.js
+++ Controllers/leads.js
@@ -429,9 +429,9 @@ export const fetchCibil = asyncHandler(async (req, res) => {
         // await cibilPdf(lead);
         // console.log(pdfResult);
 
-        const value =
-            response?.CCRResponse?.CIRReportDataLst[0]?.CIRReportData
-                ?.ScoreDetails[0]?.Value || "720";
+        const value = "720";
+        // const value = response?.CCRResponse?.CIRReportDataLst[0]?.CIRReportData
+        //     ?.ScoreDetails[0]?.Value || "720";
 
         if (!value) {
             return res.status(400).json({
